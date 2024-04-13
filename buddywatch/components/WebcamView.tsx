c0142ca7b3358b1296490bc5d44463a4b8b12bb8
Nakkeez <markus.halminen@gmail.com>
2024-04-13 12:18:55 +0300

    Implement function that sends videos to server

--- buddywatch/components/WebcamView.tsx
+++ buddywatch/components/WebcamView.tsx
@@ -34,7 +34,7 @@ export default function WebcamView() {
       }, 100);
     }
 
-    return () => {
+    return (): void => {
       if (interval) {
         clearInterval(interval);
       }
@@ -49,8 +49,8 @@ export default function WebcamView() {
       if (webcamRef.current) {
         const imageSrc: string | null | undefined =
           webcamRef.current?.getScreenshot();
-        toast.success('Screenshot taken!');
         sendImageToServer(imageSrc);
+        toast.success('Screenshot taken!');
       } else {
         toast.error('Webcam not found!');
       }
@@ -67,33 +67,36 @@ export default function WebcamView() {
    *
    * @param {string | null | undefined } imageSrc base64 encoded string of webcam image
    */
-  const sendImageToServer = (imageSrc: string | null | undefined): void => {
+  const sendImageToServer = async (
+    imageSrc: string | null | undefined
+  ): Promise<void> => {
     if (!imageSrc) return;
 
-    // Convert Base64 string to a Blob
-    fetch(imageSrc)
-      .then((res) => res.blob())
-      .then((blob) => {
-        const file: File = new File([blob], 'image.jpg', {
-          type: 'image/jpeg',
-        });
+    try {
+      // Convert Base64 string to a Blob
+      const res: Response = await fetch(imageSrc);
+      const blob: Blob = await res.blob();
 
-        const data: FormData = new FormData();
-        data.append('image', file);
-
-        fetch('http://localhost:8000/api/predict/', {
-          method: 'POST',
-          body: data,
-        })
-          .then((response: Response) => response.json())
-          .then((data): void => {
-            resizeCanvas(canvasRef, webcamRef);
-            drawPredictions(data, canvasRef);
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-          });
+      const file: File = new File([blob], 'image.jpg', {
+        type: 'image/jpeg',
       });
+
+      const data: FormData = new FormData();
+      data.append('image', file);
+
+      const response = await fetch('http://localhost:8000/api/predict/', {
+        method: 'POST',
+        body: data,
+      });
+
+      const serverResponse = await response.json();
+
+      resizeCanvas(canvasRef, webcamRef);
+      drawPredictions(serverResponse, canvasRef);
+    } catch (error) {
+      toast.error('Error while sending data to the server!');
+      console.error('Error while communicating with server:', error);
+    }
   };
 
   /**
@@ -162,6 +165,16 @@ export default function WebcamView() {
     [setRecordedChunks]
   );
 
+  const getCurrentDate = (): string => {
+    const date: Date = new Date();
+
+    const day: number = date.getDate();
+    const month: number = date.getMonth() + 1;
+    const year: number = date.getFullYear();
+
+    return `${year}-${month}-${day}`;
+  };
+
   /**
    * Download previously recorded media stream into user's device from state in webm format.
    */
@@ -174,12 +187,46 @@ export default function WebcamView() {
       const download: HTMLAnchorElement = document.createElement('a');
       document.body.appendChild(download);
       download.href = url;
-      download.download = 'react-webcam-stream-capture.webm';
+      download.download = `${getCurrentDate()}_buddywatch.webm`;
       download.click();
       window.URL.revokeObjectURL(url);
       setRecordedChunks([]);
     }
   }, [recordedChunks]);
+
+  const sendRecordingToServer = useCallback(async (): Promise<void> => {
+    if (recordedChunks.length) {
+      const blob: Blob = new Blob(recordedChunks, {
+        type: 'video/webm',
+      });
+      const formData: FormData = new FormData();
+
+      // Append the blob to form data.
+      formData.append('file', blob, `${getCurrentDate()}_buddywatch.webm`);
+      formData.append('name', 'Buddywatch Recording');
+
+      console.log(formData);
+
+      try {
+        const response = await fetch('http://localhost:8000/api/upload/', {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          toast.success('Video uploaded successfully!');
+          setRecordedChunks([]);
+        } else {
+          toast.error('Video upload failed');
+          const errorBody = await response.json();
+          console.log('Upload error:', errorBody);
+        }
+      } catch (error) {
+        console.error('Error uploading video:', error);
+        toast.error(`Error: ${error}`);
+      }
+    }
+  }, [recordedChunks, setRecordedChunks]);
 
   /**
    * Set loading state to false. Called when webcam is loaded.
@@ -196,7 +243,6 @@ export default function WebcamView() {
       {loading && (
         <div className={'flex h-screen items-center justify-center'}>
           <Watch width={100} height={100} color={'#0EA5E9'} />{' '}
-          {/* Show loading spinner */}
         </div>
       )}
       {/* Apply contentClass to conditionally toggle visibility */}
@@ -233,21 +279,39 @@ export default function WebcamView() {
             {capturing ? 'Stop Recording' : 'Start Recording'}
           </button>
           {recordedChunks.length > 0 ? (
-            <button
-              onClick={downloadRecording}
-              className="w-36 rounded-lg bg-sky-500 py-2 font-bold text-white hover:bg-sky-700"
-            >
-              Download
-            </button>
+            <>
+              <button
+                onClick={downloadRecording}
+                className="w-36 rounded-lg bg-sky-500 py-2 font-bold text-white hover:bg-sky-700"
+              >
+                Download Video
+              </button>
+              <button
+                onClick={sendRecordingToServer}
+                className="w-36 rounded-lg bg-sky-500 py-2 font-bold text-white hover:bg-sky-700"
+              >
+                Save Video
+              </button>
+            </>
           ) : (
-            <button
-              className="w-36 rounded-lg bg-gray-500 py-2 font-bold text-white"
-              onClick={() => {
-                toast.error('Nothing has been recorded!');
-              }}
-            >
-              Download
-            </button>
+            <>
+              <button
+                className="w-36 rounded-lg bg-gray-500 py-2 font-bold text-white"
+                onClick={() => {
+                  toast.error('Nothing has been recorded!');
+                }}
+              >
+                Download Video
+              </button>
+              <button
+                className="w-36 rounded-lg bg-gray-500 py-2 font-bold text-white"
+                onClick={() => {
+                  toast.error('Nothing has been recorded!');
+                }}
+              >
+                Save Video
+              </button>
+            </>
           )}
         </section>
       </section>
