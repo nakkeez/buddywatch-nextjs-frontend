9fe9835dd948732e5d17dd9b3bfad065ce699885
Nakkeez <markus.halminen@gmail.com>
2024-04-17 16:49:40 +0300

    Remove unused code

--- buddywatch/app/page.tsx
+++ buddywatch/app/page.tsx
@@ -1,21 +1,6 @@
 import WebcamView from '@/components/WebcamView';
-// import { useSession, getSession } from 'next-auth/react';
-import { getServerSession } from 'next-auth';
 
 export default async function HomePage() {
-  // const session = await getServerSession();
-  // console.log('session: ' + session?.user);
-  // const { data: session, status } = useSession();
-  // console.log(session);
-  // console.log(status);
-  // if (status === 'loading') {
-  //   return <p>Loading...</p>;
-  // }
-  //
-  // if (status === 'unauthenticated') {
-  //   return <p>Access Denied</p>;
-  // }
-
   return (
     <main className="flex h-screen flex-col items-center p-24">
       <WebcamView />
