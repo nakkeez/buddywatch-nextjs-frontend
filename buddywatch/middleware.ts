fcb010a754e30c89d86c929ff04f47a7c43f2e63
Nakkeez <markus.halminen@gmail.com>
2024-04-18 18:37:46 +0300

    Remove unused function

--- buddywatch/middleware.ts
+++ buddywatch/middleware.ts
@@ -1,16 +1,11 @@
 import { withAuth } from 'next-auth/middleware';
 
-export default withAuth(
-  function middleware(req) {
-    console.log(req.nextauth.token);
-  },
-  {
-    callbacks: {
-      authorized({ token }) {
-        return !!token?.access;
-      },
+export default withAuth({
+  callbacks: {
+    authorized({ token }) {
+      return !!token?.access;
     },
-  }
-);
+  },
+});
 
 export const config = { matcher: ['/', '/videos'] };
