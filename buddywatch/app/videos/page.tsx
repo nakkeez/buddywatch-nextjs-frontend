eed1ec656d81ebd35633428d76c72ff574e638db
Nakkeez <markus.halminen@gmail.com>
2024-04-18 18:42:21 +0300

    Add functionality to download saved videos

--- buddywatch/app/videos/page.tsx
+++ buddywatch/app/videos/page.tsx
@@ -5,6 +5,7 @@ import { useSession } from 'next-auth/react';
 import { toast } from 'react-toastify';
 import { Circles } from 'react-loader-spinner';
 import VideoItem from '@/components/VideoItem';
+import { downloadFile } from '@/utils/downloadFile';
 
 export default function VideoPage() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -22,7 +23,7 @@ export default function VideoPage() {
       }
     );
     const serverResponse = await response.json();
-    console.log(serverResponse);
+
     if (response.ok) {
       setVideos(serverResponse);
       setLoading(false);
@@ -34,6 +35,47 @@ export default function VideoPage() {
   useEffect(() => {
     if (status === 'authenticated') fetchVideos();
   }, [status]);
+
+  const downloadVideo = async (id: number) => {
+    try {
+      const response: Response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/videos/download/${id}`,
+        {
+          method: 'GET',
+          headers: {
+            authorization: `Bearer ${session?.user.access}`,
+          },
+        }
+      );
+
+      if (response.ok) {
+        const blob: Blob = await response.blob();
+        // Set default name
+        let fileName: string = 'download.webm';
+        // Get name of the file from header
+        const contentDisposition: string | null = response.headers.get(
+          'Content-Disposition'
+        );
+        if (contentDisposition) {
+          const fileNameMatch: RegExpMatchArray | null =
+            contentDisposition.match(/filename="?(.+)"?/);
+          if (fileNameMatch && fileNameMatch.length > 1)
+            fileName = fileNameMatch[1];
+        }
+        // Remove path from file name
+        fileName = fileName.replace(/^.*[\\/]/, '');
+
+        downloadFile(blob, fileName);
+        toast.success('Download started successfully!');
+      } else {
+        toast.error('Failed to download video');
+        console.log('Failed to download video');
+      }
+    } catch (error) {
+      toast.error('Failed to delete video');
+      console.error('Error while downloading video:', error);
+    }
+  };
 
   const deleteVideo = async (id: number) => {
     try {
@@ -56,27 +98,36 @@ export default function VideoPage() {
       }
     } catch (error) {
       toast.error('Failed to delete video');
-      console.error('Error deleting video:', error);
+      console.error('Error while deleting video:', error);
     }
   };
 
   const videoItems = videos
     ? videos.map((video: Video) => (
-        <VideoItem key={video.id} video={video} onDelete={deleteVideo} />
+        <VideoItem
+          key={video.id}
+          video={video}
+          onDelete={deleteVideo}
+          onDownload={downloadVideo}
+        />
       ))
     : null;
 
   return (
-    <main className="h-screen p-8">
+    <section className="h-screen p-8">
       <ul>
         {loading ? (
-          <div className={'flex h-screen justify-center'}>
-            <Circles width={100} height={100} color={'#0EA5E9'} />{' '}
+          <div
+            className={
+              'absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform'
+            }
+          >
+            <Circles width={100} height={100} color={'#0EA5E9'} />
           </div>
         ) : (
           videoItems
         )}
       </ul>
-    </main>
+    </section>
   );
 }
--- buddywatch/components/VideoItem.tsx
+++ buddywatch/components/VideoItem.tsx
@@ -4,15 +4,20 @@ import Image from 'next/image';
 interface VideoItemProps {
   video: Video;
   onDelete: (id: number) => Promise<void>;
+  onDownload: (id: number) => Promise<void>;
 }
 
-export default function VideoItem({ video, onDelete }: VideoItemProps) {
+export default function VideoItem({
+  video,
+  onDelete,
+  onDownload,
+}: VideoItemProps) {
   const formattedDate: string = new Date(video.created_at).toLocaleString(
     'fi-FI'
   );
 
   return (
-    <li className=" my-4 flex max-w-lg rounded-lg bg-white p-4 shadow-lg">
+    <li className="my-4 flex max-w-xl rounded-lg p-4 shadow shadow-slate-500 dark:bg-indigo-900">
       {video.thumbnail && (
         <Image
           src={video.thumbnail}
@@ -21,13 +26,32 @@ export default function VideoItem({ video, onDelete }: VideoItemProps) {
           height={128}
         />
       )}
-      <div className="ml-4">
-        <a href={video.file}>{video.title}</a>
+      <div className="ml-4 space-y-1.5">
+        <a
+          href={video.file}
+          className="text-lg font-extrabold text-blue-600 hover:text-blue-900 dark:text-white dark:hover:text-gray-400"
+        >
+          {video.title}
+        </a>
         <p>Created: {formattedDate}</p>
-        <p>{video.title}</p>
-        <button className="text-red-600" onClick={() => onDelete(video.id)}>
-          <Icon icon="material-symbols:delete-outline" width="24" height="24" />
-        </button>
+        <div className="flex justify-end">
+          <button
+            className="mr-2 rounded-full p-2 text-blue-800 hover:bg-gray-300 dark:bg-white dark:hover:bg-gray-400"
+            onClick={() => onDownload(video.id)}
+          >
+            <Icon icon="ic:round-download" width="32" height="32" />
+          </button>
+          <button
+            className="rounded-full p-2 text-red-600 hover:bg-gray-300 dark:bg-white dark:hover:bg-gray-400"
+            onClick={() => onDelete(video.id)}
+          >
+            <Icon
+              icon="material-symbols:delete-outline"
+              width="32"
+              height="32"
+            />
+          </button>
+        </div>
       </div>
     </li>
   );
