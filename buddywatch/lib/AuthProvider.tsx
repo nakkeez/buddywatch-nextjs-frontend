0b41effa735a80694a09c9c127d95e31748c780f
Nakkeez <markus.halminen@gmail.com>
2024-04-21 15:37:49 +0300

    refetch session on regular intervals to check token validity

--- buddywatch/lib/AuthProvider.tsx
+++ buddywatch/lib/AuthProvider.tsx
@@ -6,6 +6,15 @@ import { SessionProvider } from 'next-auth/react';
 interface AuthProviderProps {
   children: ReactNode;
 }
+
+/**
+ * Provider that wraps the application and provides authentication state.
+ * @param {AuthProviderProps} children The children of the provider
+ */
 export default function AuthProvider({ children }: AuthProviderProps) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider refetchInterval={5 * 60} refetchOnWindowFocus={true}>
+      {children}
+    </SessionProvider>
+  );
 }
